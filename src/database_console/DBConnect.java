ca20e978d33dc85a8a68f86e6d2125a0b6ed2bf1
Mrigank Doshy <mrigankdoshy@gmail.com>
2018-09-19 21:07:31 -0400

    Initial commit

---
+++ src/database_console/DBConnect.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package database_console;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBConnect 
+{
+    public static void main (String args[])
+    {
+        try
+        {
+            String host = "jdbc:derby://localhost:1527/Employees";
+            String uname = "root";
+            String pass = "root";
+            Connection con = DriverManager.getConnection(host,uname,pass);
+            
+            Statement stmt=con.createStatement();
+            String sql = "SELECT * from Workers";
+            ResultSet rs= stmt.executeQuery(sql);
+            
+            while(rs.next())
+            {
+                int id_col=rs.getInt("ID");
+                String first_name = rs.getString("First_Name");
+                String last_name = rs.getString("Last_Name");
+                String job = rs.getString("Job_Title");
+                String p= id_col+" "+first_name+" "+last_name+" "+job;
+                System.out.println(p);
+
+
+            }
+        }
+        catch (SQLException err)
+        {
+            System.out.println(err.getMessage());
+        }
+    }
+}
